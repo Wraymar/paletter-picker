d635a9fc0490deb8ada718fd942cf2a908c007ee
Wraymar <tyreesewray14@gmail.com>
2024-12-07 23:21:01 -0500

    few tweaks

--- app/src/main.js
+++ app/src/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+// handle submit
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const formValues = Object.fromEntries(new FormData(form));
+  console.log(formValues);
+  console.log(form);
+};
+
+const formButton = document.querySelector("form");
+formButton.addEventListener("submit", handleSubmit);
+
+//LOCAL STORAGE
+//set a new key
+const setLocalStorageKey = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
